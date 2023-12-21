a8bfb722a81062235cea2f05aa719750287a541d
Parth <parthdp@amazon.com>
2023-12-20 18:56:21 -0800

    add code samples for BatchDescribeEntities api for java and python

---
+++ python/src/catalog_api/entities/batch_describe_entities.py
@@ -0,0 +1,61 @@
+"""
+Purpose
+Shows how to use the AWS SDK for Python (Boto3) to describe for multiple entities information in the AWS Marketplace Catalog
+CAPI-98
+"""
+
+import json
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+PRODUCT_ID = "prod-1111111111111"
+OFFER_ID = "offer-1111111111111"
+MARKETPLACE_CATALOG = "AWSMarketplace"
+
+
+def pretty_print(response):
+    json_object = json.dumps(response, indent=4)
+    print(json_object)
+
+
+def get_entities_information(mp_client):
+    """
+    Returns information about a given product
+    Args: entity_id str: Entity to return
+    Returns: dict: Dictionary of product information
+    """
+
+    entity_request_list_param = [
+        {'EntityId': PRODUCT_ID, 'Catalog': MARKETPLACE_CATALOG},
+        {'EntityId': OFFER_ID, 'Catalog': MARKETPLACE_CATALOG}
+    ]
+    try:
+        response = mp_client.batch_describe_entities(
+            EntityRequestList=entity_request_list_param
+        )
+
+        return response
+
+    except ClientError as e:
+        logger.exception("Unexpected error: %s", e)
+        raise
+
+
+def usage_demo():
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+    print("-" * 88)
+    print("Looking for a entities in the AWS Marketplace Catalog.")
+    print("-" * 88)
+
+    mp_client = boto3.client("marketplace-catalog")
+
+    pretty_print(get_entities_information(mp_client))
+
+
+if __name__ == "__main__":
+    usage_demo()
